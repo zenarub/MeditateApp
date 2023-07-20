83f86ab3277b275d068f9179436a2faf24772d85
Zena <zena.aburub@outlook.com>
2023-07-20 16:32:08 +0300

    First Upload

--- app/src/main/java/com/example/assignment1/MainActivity.kt
+++ app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -10,35 +10,40 @@ import android.widget.Spinner
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+
 class MainActivity : AppCompatActivity() {
 
-    private val startBt : Button = findViewById(R.id.startBt)
-    private val pauseBt : Button = findViewById(R.id.pauseBt)
-    private val resetBt : Button = findViewById(R.id.resetBt)
-    private var timerTextView: TextView = findViewById(R.id.timerTextView)
+    private lateinit var startBt : Button
+    private lateinit var pauseBt : Button
+    private lateinit var resetBt : Button
+    private lateinit var timerTextView: TextView
+    private lateinit var spinX : Spinner
     var timerDuration : Long = 0
+    var remainingTime : Long = 0
     var isTimerRunning = false
     private lateinit var countDownTimer: CountDownTimer
-    private val spinX : Spinner = findViewById(R.id.spinnerTime)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        startBt = findViewById(R.id.startBt)
+        pauseBt = findViewById(R.id.pauseBt)
+        resetBt = findViewById(R.id.resetBt)
+        timerTextView= findViewById(R.id.timerTextView)
+        spinX = findViewById(R.id.spinnerTime)
 
-        val choices = arrayOf("5 minutes","10 minutes","15 minutes")
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, choices)
-        adapter.setDropDownViewResource(R.layout.spinner_item)
-        spinX.adapter = adapter
+        val choices = arrayOf(5,10,15)
+        //val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, choices)
+        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinX.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, choices)
 
         spinX.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                when (choices[p2]) {
-                    "5 minutes" -> timerDuration = 5 * 1000 * 60
-                    "10 minutes" -> timerDuration = 10 * 1000 * 60
-                    "15 minutes" -> timerDuration = 15 * 1000 * 60
-                }
-
+                val x = choices[p2]
+                timerDuration = ((x * 1000 * 60).toLong())
                 updateTimerText(timerDuration)
             }
 
@@ -50,12 +55,17 @@ class MainActivity : AppCompatActivity() {
 
         startBt.setOnClickListener{
             if (!isTimerRunning) {
-                countDownTimer = object : CountDownTimer(timerDuration, 1000) {
+                countDownTimer = object : CountDownTimer(
+                    if (remainingTime > 0) remainingTime else timerDuration,
+                    1000
+                ) {
                     override fun onTick(millisUntilFinished: Long) {
-                        updateTimerText(millisUntilFinished)
+                        remainingTime = millisUntilFinished
+                        updateTimerText(remainingTime)
                     }
 
                     override fun onFinish() {
+                        remainingTime = 0
                         updateTimerText(0)
                         isTimerRunning = false
                     }
@@ -75,11 +85,12 @@ class MainActivity : AppCompatActivity() {
         resetBt.setOnClickListener{
             countDownTimer.cancel()
             isTimerRunning = false
+            remainingTime = 0L
             updateTimerText(timerDuration)
         }
 
     }
-    private fun updateTimerText(millisUntilFinished: Long) {
+    private fun updateTimerText(millisUntilFinished: Long){
         val minutes = millisUntilFinished / 1000 / 60
         val seconds = (millisUntilFinished / 1000) % 60
         val time = String.format("%02d:%02d", minutes, seconds)
